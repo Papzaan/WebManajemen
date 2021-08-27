b7693d303f4ab3b31e60e12ae5908bee5dec5a1b
MahesaDS2408 <mahesadarmasatria@gmail.com>
2021-08-27 09:56:19 +0700

    Create asldatabase.sql
    databases

---
+++ asldatabase.sql
@@ -0,0 +1,400 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 27 Agu 2021 pada 04.55
+-- Versi server: 10.4.20-MariaDB
+-- Versi PHP: 7.3.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `asldatabase`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `no_telp` varchar(16) NOT NULL,
+  `alamat` varchar(250) NOT NULL,
+  `jenis_kelamin` enum('laki - laki','perempuan') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `id_user`, `nama`, `no_telp`, `alamat`, `jenis_kelamin`) VALUES
+(1, 1, 'Mahesa Darma Satria', '088274090609', 'Serbajadi 1 rt 01 rw 01 Kec.Natar Kab.Lampung Selatan Lampung', 'laki - laki');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `id_sup` int(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `tgl_masuk` date NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `harga` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang_mitra`
+--
+
+CREATE TABLE `barang_mitra` (
+  `id_barmit` int(11) NOT NULL,
+  `id_mitra` int(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `tgl_masuk` date NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `harga` varchar(30) NOT NULL,
+  `status` enum('lunas','belum lunas') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `customer`
+--
+
+CREATE TABLE `customer` (
+  `id_customer` int(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `no_telp` varchar(16) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  `foto_ktp` varchar(30) NOT NULL,
+  `foto_customer` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `mitra`
+--
+
+CREATE TABLE `mitra` (
+  `id_mitra` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `no_telp` varchar(16) NOT NULL,
+  `alamat` varchar(250) NOT NULL,
+  `jenis_kelamin` enum('laki - laki','perempuan') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `mitra`
+--
+
+INSERT INTO `mitra` (`id_mitra`, `id_user`, `nama`, `nik`, `no_telp`, `alamat`, `jenis_kelamin`) VALUES
+(1, 3, 'Imam Haris', '182029283029', '08282939282', 'bandar jaya, lampung tengah, lampung', 'laki - laki');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `id_sales` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `tgl_penjualan` date NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `harga` varchar(30) NOT NULL,
+  `alamat_trank` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `penjualan_mitra`
+--
+
+CREATE TABLE `penjualan_mitra` (
+  `id_penjumit` int(11) NOT NULL,
+  `id_mitra` int(11) NOT NULL,
+  `id_customer` int(11) NOT NULL,
+  `id_barmit` int(11) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `tgl_jual` date NOT NULL,
+  `harga` int(30) NOT NULL,
+  `alamat_trank` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `sales`
+--
+
+CREATE TABLE `sales` (
+  `id_sales` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `nik` varchar(20) NOT NULL,
+  `no_telp` varchar(16) NOT NULL,
+  `alamat` varchar(250) NOT NULL,
+  `jenis_kelamin` enum('laki - laki','perempuan') DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `sales`
+--
+
+INSERT INTO `sales` (`id_sales`, `id_user`, `nama`, `nik`, `no_telp`, `alamat`, `jenis_kelamin`) VALUES
+(1, 2, 'Aan Sanova', '180027199271927', '0883928199208', 'sukabumi lampung, lampung', 'laki - laki');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `suplayer`
+--
+
+CREATE TABLE `suplayer` (
+  `id_sup` int(11) NOT NULL,
+  `nama` varchar(25) NOT NULL,
+  `no_telp` varchar(16) NOT NULL,
+  `alamat` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `status` enum('sales','mitra','admin') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `user`
+--
+
+INSERT INTO `user` (`id_user`, `email`, `password`, `status`) VALUES
+(1, 'mahesadarmasatria@gmail.com', 'mahesa123', 'admin'),
+(2, 'aansanova@gmail.com', 'aansan123', 'mitra'),
+(3, 'imamharis@gmail.com', 'imamhar123', 'sales');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`),
+  ADD KEY `id_sup` (`id_sup`);
+
+--
+-- Indeks untuk tabel `barang_mitra`
+--
+ALTER TABLE `barang_mitra`
+  ADD PRIMARY KEY (`id_barmit`),
+  ADD KEY `id_mitra` (`id_mitra`);
+
+--
+-- Indeks untuk tabel `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`id_customer`);
+
+--
+-- Indeks untuk tabel `mitra`
+--
+ALTER TABLE `mitra`
+  ADD PRIMARY KEY (`id_mitra`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`),
+  ADD KEY `id_customer` (`id_customer`),
+  ADD KEY `id_sales` (`id_sales`),
+  ADD KEY `id_barang` (`id_barang`);
+
+--
+-- Indeks untuk tabel `penjualan_mitra`
+--
+ALTER TABLE `penjualan_mitra`
+  ADD PRIMARY KEY (`id_penjumit`),
+  ADD KEY `id_mitra` (`id_mitra`),
+  ADD KEY `id_customer` (`id_customer`),
+  ADD KEY `id_barmit` (`id_barmit`);
+
+--
+-- Indeks untuk tabel `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id_sales`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `suplayer`
+--
+ALTER TABLE `suplayer`
+  ADD PRIMARY KEY (`id_sup`);
+
+--
+-- Indeks untuk tabel `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `barang_mitra`
+--
+ALTER TABLE `barang_mitra`
+  MODIFY `id_barmit` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `id_customer` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `mitra`
+--
+ALTER TABLE `mitra`
+  MODIFY `id_mitra` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `penjualan_mitra`
+--
+ALTER TABLE `penjualan_mitra`
+  MODIFY `id_penjumit` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id_sales` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `suplayer`
+--
+ALTER TABLE `suplayer`
+  MODIFY `id_sup` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `user`
+--
+ALTER TABLE `user`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `id_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Ketidakleluasaan untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD CONSTRAINT `barang_ibfk_1` FOREIGN KEY (`id_sup`) REFERENCES `suplayer` (`id_sup`);
+
+--
+-- Ketidakleluasaan untuk tabel `barang_mitra`
+--
+ALTER TABLE `barang_mitra`
+  ADD CONSTRAINT `barang_mitra_ibfk_1` FOREIGN KEY (`id_mitra`) REFERENCES `mitra` (`id_mitra`);
+
+--
+-- Ketidakleluasaan untuk tabel `mitra`
+--
+ALTER TABLE `mitra`
+  ADD CONSTRAINT `mitra_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Ketidakleluasaan untuk tabel `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD CONSTRAINT `penjualan_ibfk_1` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id_customer`),
+  ADD CONSTRAINT `penjualan_ibfk_2` FOREIGN KEY (`id_sales`) REFERENCES `sales` (`id_sales`),
+  ADD CONSTRAINT `penjualan_ibfk_3` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`);
+
+--
+-- Ketidakleluasaan untuk tabel `penjualan_mitra`
+--
+ALTER TABLE `penjualan_mitra`
+  ADD CONSTRAINT `penjualan_mitra_ibfk_1` FOREIGN KEY (`id_barmit`) REFERENCES `barang_mitra` (`id_barmit`),
+  ADD CONSTRAINT `penjualan_mitra_ibfk_2` FOREIGN KEY (`id_mitra`) REFERENCES `mitra` (`id_mitra`),
+  ADD CONSTRAINT `penjualan_mitra_ibfk_3` FOREIGN KEY (`id_customer`) REFERENCES `customer` (`id_customer`);
+
+--
+-- Ketidakleluasaan untuk tabel `sales`
+--
+ALTER TABLE `sales`
+  ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
