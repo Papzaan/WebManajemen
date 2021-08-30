18f294a2ec878be04741f302c4c4f7f5ccdc514c
Aan Sanova <37663944+Papzaan@users.noreply.github.com>
2021-08-30 11:42:17 +0700

    Register

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Login</title>
+    <title>Login</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?php base_url(); ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
@@ -42,11 +42,11 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Selamat Datang!</h1>
                                     </div>
                                     <form class="user" method="post" action="/auth/valid_login">
                                         <div class="form-group">
-                                            <input type="text" class="form-control form-control-user" name="email" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                                            <input type="text" class="form-control form-control-user" name="email" aria-describedby="emailHelp" placeholder="Masukan Alamat Email...">
                                         </div>
                                         <div class="form-group">
                                             <input type="password" class="form-control form-control-user" name="password" placeholder="Password">
@@ -54,25 +54,19 @@
                                         <div class="form-group">
                                             <div class="custom-control custom-checkbox small">
                                                 <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
+                                                <label class="custom-control-label" for="customCheck">Ingat Saya</label>
                                             </div>
                                         </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block" name="login">Login</button>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block" name="login">Masuk</button>
                                         <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
+
                                     </form>
                                     <hr>
                                     <div class="text-center">
-                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                        <a class="small" href="forgot-password.html">Lupa Password?</a>
                                     </div>
                                     <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
+                                        Belum Punya Akun? <a class="small" href="/auth/register">Klik Disini!</a>
                                     </div>
                                 </div>
                             </div>
--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -1,35 +1,157 @@
-<html>
-    <head>
-        <title>Tutorial Login Dan Register</title>
-    </head>
-    <body>
-        <?php 
-            $session = session();
-            $error = $session->getFlashdata('error');
-        ?>
-        
-        <?php if($error){ ?>
-            <p style="color:red">Terjadi Kesalahan:
-                <ul>
-                    <?php foreach($error as $e){ ?>
-                    <li><?php echo $e ?></li>
-                    <?php } ?>
-                </ul>
-            </p>
-        <?php } ?>
-        
-        <h5>Register</h5>
-        <form method="post" action="/auth/valid_register">
-            Username: <br>
-            <input type="text" name="email" required><br>
-            Password: <br>
-            <input type="password" name="password" required><br>
-            Konfirmasi Password: <br>
-            <input type="password" name="confirm" required><br>
-            <button type="submit" name="login">Register</button>
-        </form>
-        <p>
-            <a href="/auth/login">Sudah punya akun?</a>
-        </p>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SB Admin 2 - Register</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="<?php base_url(); ?>/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+    <!-- Custom styles for this template-->
+    <link href="<?php base_url(); ?>/assets/css/sb-admin-2.min.css" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card-body p-0">
+                <!-- Nested Row within Card Body -->
+                <div class="row">
+                    <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+                    <div class="col-lg-7">
+                        <div class="p-5">
+                            <!-- tab -->
+                            <div class="text-center">
+                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+                            </div>
+                            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                                <?php $p = @$_GET['p']; ?>
+                                <li class="nav-item" role="presentation">
+                                    <a class="nav-link <?php echo $p == '' || $p == 'mitra' ? 'active' : ''; ?>" href="/auth/register?p=mitra">Mitra</a>
+                                </li>
+                                <li class="nav-item" role="presentation">
+                                    <a class="nav-link <?php echo $p == 'sales' ? 'active' : ''; ?>" href="/auth/register?p=sales">Sales</a>
+                                </li>
+
+                            </ul>
+                            <div class="tab-content" id="myTabContent">
+                                <?php if ($p == '' || $p == 'mitra') { ?>
+                                    <div class="tab-pane fade show active">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900 mb-4">Daftar Mitra</h1>
+                                        </div>
+                                        <form class="user">
+                                            <div class="form-group row">
+                                                <div class="col-sm-6 mb-3 mb-sm-0">
+                                                    <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                                            </div>
+                                            <div class="form-group row">
+                                                <div class="col-sm-6 mb-3 mb-sm-0">
+                                                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
+                                                </div>
+                                            </div>
+                                            <a href="login.html" class="btn btn-primary btn-user btn-block">
+                                                Register Account
+                                            </a>
+                                            <hr>
+                                            <a href="index.html" class="btn btn-google btn-user btn-block">
+                                                <i class="fab fa-google fa-fw"></i> Register with Google
+                                            </a>
+                                            <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                                <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
+                                            </a>
+                                        </form>
+                                        <hr>
+                                        <div class="text-center">
+                                            <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                        </div>
+                                        <div class="text-center">
+                                            <a class="small" href="login.html">Already have an account? Login!</a>
+                                        </div>
+                                    </div>
+                                <?php } elseif ($p == 'sales') { ?>
+                                    <div class="tab-pane fade show active">
+                                        <div class="text-center">
+                                            <h1 class="h4 text-gray-900 mb-4">Daftar Sales</h1>
+                                        </div>
+                                        <form class="user">
+                                            <div class="form-group row">
+                                                <div class="col-sm-6 mb-3 mb-sm-0">
+                                                    <input type="text" class="form-control form-control-user" id="exampleFirstName" placeholder="First Name">
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <input type="text" class="form-control form-control-user" id="exampleLastName" placeholder="Last Name">
+                                                </div>
+                                            </div>
+                                            <div class="form-group">
+                                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" placeholder="Email Address">
+                                            </div>
+                                            <div class="form-group row">
+                                                <div class="col-sm-6 mb-3 mb-sm-0">
+                                                    <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <input type="password" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Repeat Password">
+                                                </div>
+                                            </div>
+                                            <a href="login.html" class="btn btn-primary btn-user btn-block">
+                                                Register Account
+                                            </a>
+                                            <hr>
+                                        </form>
+                                        <hr>
+                                        <div class="text-center">
+                                            <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                        </div>
+                                        <div class="text-center">
+                                            <a class="small" href="login.html">Already have an account? Login!</a>
+                                        </div>
+                                    </div>
+                                <?php } else { ?>
+                                    <div class="tab-pane fade show active">Contact</div>
+                                <?php } ?>
+                            </div>
+                            <!-- tab -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="<?php base_url(); ?>/assets/vendor/jquery/jquery.min.js"></script>
+    <script src="<?php base_url(); ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="<?php base_url(); ?>/assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="<?php base_url(); ?>/assets/js/sb-admin-2.min.js"></script>
+
+</body>
+
 </html>
