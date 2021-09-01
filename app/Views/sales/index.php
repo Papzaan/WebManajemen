1b01172c8df9f70f9db9bc9210c9d79b0d7e7c24
Aan Sanova <37663944+Papzaan@users.noreply.github.com>
2021-09-01 15:16:43 +0700

    salses

--- app/Views/sales/index.php
+++ app/Views/sales/index.php
@@ -1,41 +1,68 @@
-<html>
-    <head>
-        <title>halaman Sales</title>
-    </head>
-    <body>
-        <?php $session = session() ?>
-        <h4>Selamat datang Sales!</h4>
-        <?php echo $session->get('email')?>
-        <a href="/auth/login">Logout</a>
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+  <!-- Page Heading -->
+  <h1 class="h3 mb-2 text-gray-800">Mitra</h1>
+
+  <!-- DataTales Example -->
+  <div class="card shadow mb-4">
+    <div class="card-header py-3">
+      <!-- <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6> -->
+      <a href="#"><button class="btn btn-primary float-right" type="button">
+          Tambah Data <i class="fas fa-plus"></i>
+        </button></a>
+    </div>
+    <div class="card-body">
+      <div class="table-responsive">
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+          <thead>
+            <tr>
+              <th>NO</th>
+              <th>Nama</th>
+              <th>NIK</th>
+              <th>no_telp</th>
+              <th>Alamat</th>
+              <th>jenis kelamin</th>
+              <th>Email</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <?php
+            $no = 1;
+            foreach ($user as $d) {
+            ?>
+              <tr>
+                <td><?php echo $no++ ?></td>
+                <td><?php echo $d["nama"] ?></td>
+                <td><?php echo $d["nik"] ?></td>
+                <td><?php echo $d["no_telp"] ?></td>
+                <td><?php echo $d["alamat"] ?></td>
+                <td><?php echo $d["jenis_kelamin"] ?></td>
+                <td><?php echo $d["email"] ?></td>
+              </tr>
+            <?php } ?>
 
 
-  <h1 align="center">Tabel Profil</h1>
-  <table border="1" width="80%" align="center">
-    <tr>
-      <th>NO</th>
-      <th>Nama</th>
-      <th>NIK</th>
-      <th>no_telp</th>
-      <th>Alamat</th>
-      <th>jenis kelamin</th>
-      <th>Email</th>
-      <th>Status</th>
-    </tr>
-    <?php
-      $no = 1;
-      foreach($user as $d){
-    ?>
-    <tr>
-      <td><?php echo $no++ ?></td>
-      <td><?php echo $d["nama"] ?></td>
-      <td><?php echo $d["nik"] ?></td>
-      <td><?php echo $d["no_telp"] ?></td>
-      <td><?php echo $d["alamat"] ?></td>
-      <td><?php echo $d["jenis_kelamin"] ?></td>
-      <td><?php echo $d["email"] ?></td>
-    </tr>
-  <?php } ?>
-        
+            </tr>
 
-    </body>
-</html>
+            </tr>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+</div>
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
+
+<?= $this->endSection(); ?>
