5ee768c77d4702b35603c64b4fe3218f4fa4e083
Aan Sanova <37663944+Papzaan@users.noreply.github.com>
2021-09-03 14:25:54 +0700

    Update index.php

--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -15,7 +15,7 @@
     <!-- Content Row -->
     <div class="row">
 
-        <!-- Earnings (Monthly) Card Example -->
+        <!-- Barang (Total) Card -->
         <div class="col-xl-3 col-md-6 mb-4">
             <div class="card border-left-primary shadow h-100 py-2">
                 <div class="card-body">
