451f805e8a0242e1009296590fc6aadc3817cbfc
Aan Sanova <37663944+Papzaan@users.noreply.github.com>
2021-09-06 14:28:40 +0700

    Update index.php

--- app/Views/penjualan/index.php
+++ app/Views/penjualan/index.php
@@ -15,13 +15,14 @@
                 <?php foreach ($barang as $d) { ?>
                     <div class="col-lg-3">
                         <div class="card">
-                            <div class="card_body text-center">
+                            <div class="card-body text-center">
                                 <h5><?php echo $d["nama"] ?></h5>
                                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-
+                                <p class="card-text">
+                                    <img src="https://majushop.id/image?id=69507.jpg&option=2&t=1626403350" width="200px" height="150px">
+                                </p>
                                 <label> <?php echo number_to_currency($d['harga'], 'IDR'); ?></label><br>
                                 <button class="btn btn-success btn-sm"><i class="fa fa-shopping-basket"></i>Add</button>
-
                             </div>
                         </div>
                     </div>
