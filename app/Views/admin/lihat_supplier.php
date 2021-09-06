241ad2a19938401ab8d53cb8532b3b91609813df
Aan Sanova <37663944+Papzaan@users.noreply.github.com>
2021-09-06 12:00:01 +0700

    Update lihat_supplier.php

--- app/Views/admin/lihat_supplier.php
+++ app/Views/admin/lihat_supplier.php
@@ -37,21 +37,22 @@
                             $no = 1;
                             foreach ($barang as $d) {
                             ?>
-                        <tr>
-                            <td><?php echo $no++ ?></td>
-                            <td><?php echo $d["nama_sup"] ?></td>
-                            <td><?php echo $d["no_telp"] ?></td>
-                            <td><?php echo $d["alamat"] ?></td>
-                            <td>
-                                <a href="#"> <button class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i> Edit</button>
-                                </a>
-                                <a href="#"> <button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"> Hapus</i></button>
 
-                                </a>
-                            </td>
-                        <?php } ?>
+                                <td><?php echo $no++ ?></td>
+                                <td><?php echo $d["nama_sup"] ?></td>
+                                <td><?php echo $d["no_telp"] ?></td>
+                                <td><?php echo $d["alamat"] ?></td>
+                                <td>
+                                    <a href="#"> <button class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i> Edit</button>
+                                    </a>
+                                    <a href="#"> <button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"> Hapus</i></button>
 
+                                    </a>
+                                </td>
                         </tr>
+                    <?php } ?>
+
+
 
                     </tbody>
                 </table>
