d9e8234130b809d8cf3fb653d52ab4b8691d8b4d
Aan Sanova <37663944+Papzaan@users.noreply.github.com>
2021-09-10 10:51:04 +0700

    tipis

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -19,7 +19,6 @@
     <link href="<?php base_url(); ?>/assets/css/sb-admin-2.min.css" rel="stylesheet">
 
     <!-- Custom styles for this page -->
-    <link href="<?php base_url(); ?>/assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
     <link rel="stylesheet" href="<?php base_url(); ?>/assets/css/jquery-ui.css">
 
 
@@ -392,10 +391,6 @@
 
         <!-- Custom scripts for all pages-->
         <script src="<?php base_url(); ?>/assets/js/sb-admin-2.min.js"></script>
-
-        <!-- Page level plugins -->
-        <script src="<?php base_url(); ?>/assets/vendor/datatables/jquery.dataTables.min.js"></script>
-        <script src="<?php base_url(); ?>/assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
         <!-- Page level custom scripts -->
 
