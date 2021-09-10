a7d0dfbb620a70b17145fb3b7c7b0db9c548ff00
MahesaDS2408 <mahesadarmasatria@gmail.com>
2021-09-10 08:21:27 +0700

    Update DataSales.php

--- app/Controllers/DataSales.php
+++ app/Controllers/DataSales.php
@@ -13,24 +13,6 @@ class DataSales extends BaseController
         $this->session = session();
     }
 
-    /*public function index()
-    {
-        //cek apakah ada session bernama isLogin
-        if (!$this->session->has('isLogin')) {
-            return redirect()->to('/auth/login');
-        }
-
-        //cek role dari session
-        if ($this->session->get('status') != 1) {
-            return redirect()->to('/user');
-        }
-        //tampilin data
-        $model = new UserModel();
-        $data['user'] = $model->tampilmitra();
-        // echo view('admin/datamitra',$data);
-        return view('admin/lihat_mitra');
-    }*/
-
     public function tampil()
     {
         //cek apakah ada session bernama isLogin
