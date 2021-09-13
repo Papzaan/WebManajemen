4af68482b86977a7d0259d56b87f5bfbc764ce8d
Aan Sanova <37663944+Papzaan@users.noreply.github.com>
2021-09-13 11:03:29 +0700

    Update Mitra.php

--- app/Controllers/Mitra.php
+++ app/Controllers/Mitra.php
@@ -32,4 +32,24 @@ class Mitra extends BaseController
 
 
     }
+    public function profile()
+    {
+        //cek apakah ada session bernama isLogin
+        if (!$this->session->has('isLogin')) {
+            return redirect()->to('/auth/login');
+        }
+
+        //cek role dari session
+        if ($this->session->get('status') != 2) {
+            return redirect()->to('/user');
+        }
+        //tampilin data
+        $model = new UserModel();
+        $data['user'] = $model->getdataMitra();
+        $data['title'] = 'Profile';
+        echo view('mitra/profil', $data);
+        //return view('admin/index')
+
+
+    }
 }
