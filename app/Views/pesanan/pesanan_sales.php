c1a71beaac7d9ba737f825f7d880e6f0b722b279
Aan Sanova <37663944+Papzaan@users.noreply.github.com>
2021-09-13 14:41:04 +0700

    Update pesanan_sales.php

--- app/Views/pesanan/pesanan_sales.php
+++ app/Views/pesanan/pesanan_sales.php
@@ -31,16 +31,17 @@
                             <th>Alamat</th>
                             <th>pembayaran</th>
                             <th>Status</th>
-                            <!--<?php 
-                            foreach ($pessal as $d) {
-                                if($d["utang"] != "0"){?>
+                            <!--<?php
+                                foreach ($pessal as $d) {
+                                    if ($d["utang"] != "0") { ?>
                                 <th>aksi</th>
-                            <?php }}?>-->
+                            <?php }
+                                } ?>-->
                         </tr>
                     </thead>
 
                     <tbody>
-                    <?php
+                        <?php
                         $no = 1;
                         foreach ($pessal as $d) {
                         ?>
@@ -53,40 +54,69 @@
                                 <td><?php echo $d["harga"] ?></td>
                                 <td><?php echo $d["utang"] ?></td>
                                 <td><?php echo $d["alamat"] ?></td>
-                                <?php 
-                                if($d["bayar"] == "1"){?>
+                                <?php
+                                if ($d["bayar"] == "1") { ?>
                                     <td><span class="bg-gradient-success text-white">1 Kali</span></td>
-                                <?php }?>
-                                <?php 
-                                if($d["bayar"] == "2"){?>
+                                <?php } ?>
+                                <?php
+                                if ($d["bayar"] == "2") { ?>
                                     <td><span class="bg-gradient-warning text-white">2 Kali</span></td>
-                                <?php }?>
-                                <?php 
-                                if($d["bayar"] == "3"){?>
+                                <?php } ?>
+                                <?php
+                                if ($d["bayar"] == "3") { ?>
                                     <td><span class="bg-gradient-warning text-white">3 Kali</span></td>
-                                <?php }?>
-                                <?php 
-                                if($d["utang"] == "0"){?>
-                                <td><span class="bg-gradient-success text-white">Lunas</span></td>
-                                <?php }?>
-                                <?php 
-                                if($d["utang"] != "0"){?>
-                                <td><span class="bg-gradient-danger text-white">Belum Lunas</span>
-                                    <a href="#"><button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"> aksi</i></button>
-                                </td>
-                                <?php }?>
-                                <!--<?php 
-                                if($d["utang"] != "0"){?>
+                                <?php } ?>
+                                <?php
+                                if ($d["utang"] == "0") { ?>
+                                    <td><span class="bg-gradient-success text-white">Lunas</span></td>
+                                <?php } ?>
+                                <?php
+                                if ($d["utang"] != "0") { ?>
+                                    <td><span class="bg-gradient-danger text-white">Belum Lunas</span>
+                                        <a href="#"><button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="modal" data-placement="top" title="Delete" data-target="#myModal"><i class="fa fa-trash"> aksi</i></button>
+                                    </td>
+                                <?php } ?>
+                                <!--<?php
+                                    if ($d["utang"] != "0") { ?>
                                  <td><a href="#"><button class="btn btn-success btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Edit"><i class="fa fa-edit"></i> Lunas</button>
                                     <a href="#"><button class="btn btn-danger btn-sm rounded-0" type="button" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-trash"> Belum Lunas</i></button>
                                 </td>
-                                <?php }?>-->
-                              
+                                <?php } ?>-->
+
                             </tr>
                         <?php } ?>
 
                     </tbody>
                 </table>
+
+                <!-- The Modal -->
+                <div class="modal" id="myModal">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+
+                            <!-- Modal Header -->
+                            <div class="modal-header">
+                                <h4 class="modal-title">Pembayaran</h4>
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                            </div>
+
+                            <!-- Modal body -->
+                            <div class="modal-body">
+                                <form action="">
+                                    <input type="text" name="bayar" id="bayar">
+                                    <button type="button" class="btn btn-success" data-dismiss="modal">Bayar</button>
+                                </form>
+                            </div>
+
+                            <!-- Modal footer -->
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
