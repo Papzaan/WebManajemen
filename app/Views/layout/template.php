3695a5adf867a89a44f6a6e4c9bd05c0c8abdfcf
Aan Sanova <37663944+Papzaan@users.noreply.github.com>
2021-09-13 15:54:07 +0700

    Update template.php

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -21,9 +21,7 @@
     <!-- Custom styles for this page -->
     <link rel="stylesheet" href="<?php base_url(); ?>/assets/css/jquery-ui.css">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 
 
 </head>
