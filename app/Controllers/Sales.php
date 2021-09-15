0461c433d686283c1720ab642f58ba1819db7ce4
MahesaDS2408 <mahesadarmasatria@gmail.com>
2021-09-15 14:42:53 +0700

    Update Sales.php

--- app/Controllers/Sales.php
+++ app/Controllers/Sales.php
@@ -87,7 +87,7 @@ class Sales extends BaseController
         //tampilin data
         $model = new UserModel();
         $data['title'] = 'Tambah Pesanan Sales';
-        $data['user'] = $model->getdataAdmin();
+        $data['user'] = $model->getdataSales();
         $model = new StokModel();
         $data['kategori'] = $model->getstok();
         echo view('sales/tambah_pesanan', $data);
@@ -112,9 +112,53 @@ class Sales extends BaseController
         $stok = $model->editstokju($kate);
         $pesan = $data['jumlah'];
         
-        //$harga = $data['harga_total'];
-        var_dump($kate);
-        var_dump($pesan);
-        var_dump($data['harga_total']);
+        if($stok == 0){
+            // kirim peringatan 
+            session()->setFlashdata('stok_habis',  '<div class="alert alert-danger text-center">Stok Habis!!!!</div>');
+            return redirect()->to('/sales/tambah_pes');
+        } else if($stok < $pesan){
+            // kirim peringatan 
+            session()->setFlashdata('stok_habis',  '<div class="alert alert-danger text-center">Stok Kurang Dari Pesanan!!!!</div>');
+            return redirect()->to('/sales/tambah_pes');
+        } else {
+                    //masukan catatan penjualan
+                    $model1 = new UserModel();
+                    $id_admin = $model1->getdataidAdmin();
+                    $nama = $data['nama_cus'];
+                    $model2 = new UserCustomer();
+                    $nik = $model2->getnikCustomer($nama);
+                    //var_dump($nik);
+
+                    $this->penjualanModel = new PenjualanModel();
+                    $this->penjualanModel->save([
+                        'id_admin' => $id_admin,
+                        'nik_customer' => $nik,
+                        'nama_kategori' => $data['nama_kategori'],
+                        'tgl_jual' => $data['tgl_jual'],
+                        'jumlah' => $data['jumlah'],
+                        'harga' => $data['harga'],
+                        'alamat_trank' => $data['alamat'],
+                        'status' => "lunas"
+                    ]);
+                    //var_dump($stok);
+                    //var_dump($data['jumlah']);
+                    //jumlahkan
+                    $upjum = $stok - $data['jumlah'];
+                    //$upjum = 10 + $data['jumlah'];
+                    //update stoknya
+                    $dataupdate = [
+                        'stok' => $upjum
+                    ];
+                    $kat = $data['nama_kategori'];
+                    
+                    $update = $this->stokModel->updatejumstok($dataupdate, $kat);
+                    // Jika berhasil melakukan ubah
+                    if($update)
+                    {
+                        return redirect()->to('/penjualan/catatan');
+                        
+                    }
+                }
+
     }
 }
