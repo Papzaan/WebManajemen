56ddf8eec715ca38678b59666ee0223706e1aadd
ImamHaris <iamimam0107@gmail.com>
2021-09-25 16:11:15 +0700

    Update template.php

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -132,12 +132,7 @@
                 </li>
 
 
-                <!-- Nav Item - Charts -->
-                <li class="nav-item">
-                    <a class="nav-link" href="charts.html">
-                        <i class="fas fa-fw fa-chart-area"></i>
-                        <span>Charts</span></a>
-                </li>
+                
             <?php } ?>
 
             <?php if ($this->session->get('status') == 2) {
