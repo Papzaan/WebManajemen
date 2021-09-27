ea6c990b1ac3ec415ce8ef74d9cc60871a3dc6e4
Aan Sanova <37663944+Papzaan@users.noreply.github.com>
2021-09-27 11:16:26 +0700

    Update laporan_penjualan_sales.php

--- app/Views/penjualan/laporan_penjualan_sales.php
+++ app/Views/penjualan/laporan_penjualan_sales.php
@@ -12,7 +12,7 @@
     <div class="card shadow mb-4">
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-bordered table-hover" id="dataTableCatatanPenjualan" width="100%" cellspacing="0">
+                <table class="table table-bordered table-hover" id="dataTableCatatanPenjualanSales" width="100%" cellspacing="0">
                     <thead>
                         <tr>
                             <th>No.</th>
@@ -27,21 +27,21 @@
                     </thead>
 
                     <tbody>
-                    <?php
-                            $no = 1;
-                            foreach ($pensal as $d) {
-                            ?>
-                                <tr id="">
-                                    <td><?php echo $no++ ?></td>
-                                    <td><?php echo $d["nama"] ?></td>
-                                    <td><?php echo $d["nama_cussal"] ?></td>
-                                    <td><?php echo $d["nama_kategori"] ?></td>
-                                    <td><?php echo $d["tgl_jual"] ?></td>
-                                    <td><?php echo $d["jumlah"] ?></td>
-                                    <td><?php echo $d["harga"] ?></td>
-                                    <td><?php echo $d["alamat_trank"] ?></td>
-                                </tr>
-                            <?php } ?>
+                        <?php
+                        $no = 1;
+                        foreach ($pensal as $d) {
+                        ?>
+                            <tr id="">
+                                <td><?php echo $no++ ?></td>
+                                <td><?php echo $d["nama"] ?></td>
+                                <td><?php echo $d["nama_cussal"] ?></td>
+                                <td><?php echo $d["nama_kategori"] ?></td>
+                                <td><?php echo $d["tgl_jual"] ?></td>
+                                <td><?php echo $d["jumlah"] ?></td>
+                                <td><?php echo $d["harga"] ?></td>
+                                <td><?php echo $d["alamat_trank"] ?></td>
+                            </tr>
+                        <?php } ?>
                     </tbody>
                 </table>
             </div>
