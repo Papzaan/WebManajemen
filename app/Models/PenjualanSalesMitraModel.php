0a6a74332245285da179ba8cd6dbca7e056863f1
MahesaDS2408 <mahesadarmasatria@gmail.com>
2021-09-28 08:35:15 +0700

    fix penjualan salesnya mitra

--- app/Models/PenjualanSalesMitraModel.php
+++ app/Models/PenjualanSalesMitraModel.php
@@ -8,7 +8,7 @@ class PenjualanSalesMitraModel extends Model
 {
     protected $table = "penjualan_salmit";
     protected $primaryKey = "id_penjualan";
-    protected $allowedFields = ["id_salmit", "nik_customer_salmit","id_stokbarmit","jumlah", "tgl_jual","harga","alamat_trank", "status"];
+    protected $allowedFields = ["id_salmit", "no_telp_customer_salmit","id_stokbarmit","jumlah", "tgl_jual","harga","alamat_trank", "status"];
     protected $useTimestamps = false;
     
     public function getpenjualansalmit(){
@@ -18,8 +18,8 @@ class PenjualanSalesMitraModel extends Model
         ->join('stok_barang_mitra','stok_barang_mitra.id_stokbarmit=penjualan_salmit.id_stokbarmit')
         ->join('salesnya_mitra','salesnya_mitra.id_salmit=penjualan_salmit.id_salmit')
         ->join('mitra','mitra.id_mitra=salesnya_mitra.id_mitra')
-        ->join('customer_salmit','customer_salmit.nik_customer_salmit=penjualan_salmit.nik_customer_salmit')
-        ->select('penjualan_salmit.jumlah, penjualan_salmit.tgl_jual, penjualan_salmit.harga, penjualan_salmit.alamat_trank, salesnya_mitra.nama_salmit, stok_barang_mitra.nama_kategori, customer_salmit.nama_cussalmit')
+        ->join('customer_salmit','customer_salmit.no_telp_customer_salmit=penjualan_salmit.no_telp_customer_salmit')
+        ->select('penjualan_salmit.jumlah, penjualan_salmit.tgl_jual, penjualan_salmit.harga, penjualan_salmit.alamat_trank, salesnya_mitra.nama_salmit, stok_barang_mitra.nama_kategori, customer_salmit.nama_cussalmit, customer_salmit.no_telp_customer_salmit')
         ->where('mitra.email',['email'=> $data])
         ->get()->getResultArray();
     }
