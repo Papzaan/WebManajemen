f84a59405315410913187702585e06a05387c2de
MahesaDS2408 <mahesadarmasatria@gmail.com>
2021-09-28 12:33:28 +0700

    Update index.php

--- app/Views/salesnyamitra/index.php
+++ app/Views/salesnyamitra/index.php
@@ -15,79 +15,55 @@
   <!-- Content Row -->
   <div class="row">
 
-    <!-- Barang (Total) Card -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-primary shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                Total Penjualan</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">4000</div>
+      <!-- Barang (Total) Card -->
+      <div class="col-xl-6 col-md-6 mb-4">
+        <div class="card border-left-primary shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                  Total Penjualan</div>
+                <div class="h5 mb-0 font-weight-bold text-gray-800">4000</div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-calendar fa-2x text-gray-300"></i>
+              </div>
             </div>
-            <div class="col-auto">
-              <i class="fas fa-calendar fa-2x text-gray-300"></i>
-            </div>
-          </div>
-          <a href="" class="small-box-footer">
+            <a href="#" class="small-box-footer">
 
-            <i class="fa fa-arrow-circle-right"></i>
-          </a>
+              <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
 
-    <!-- Earnings (Monthly) Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-info shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Kategori
-              </div>
-              <div class="row no-gutters align-items-center">
-                <div class="col-auto">
-                  <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50</div>
+      <!-- Earnings (Monthly) Card Example -->
+      <div class="col-xl-6 col-md-6 mb-4">
+        <div class="card border-left-info shadow h-100 py-2">
+          <div class="card-body">
+            <div class="row no-gutters align-items-center">
+              <div class="col mr-2">
+                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Kategori
                 </div>
+                <div class="row no-gutters align-items-center">
+                  <div class="col-auto">
+                    <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50</div>
+                  </div>
 
+                </div>
+              </div>
+              <div class="col-auto">
+                <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
               </div>
             </div>
-            <div class="col-auto">
-              <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
-            </div>
-          </div>
-          <a href="" class="small-box-footer">
+            <a href="#" class="small-box-footer">
 
-            <i class="fa fa-arrow-circle-right"></i>
-          </a>
+              <i class="fa fa-arrow-circle-right"></i>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
 
-    <!-- Pending Requests Card Example -->
-    <div class="col-xl-3 col-md-6 mb-4">
-      <div class="card border-left-warning shadow h-100 py-2">
-        <div class="card-body">
-          <div class="row no-gutters align-items-center">
-            <div class="col mr-2">
-              <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                Total Stok Barang</div>
-              <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
-
-            </div>
-
-            <div class="col-auto">
-              <i class="fas fa-retweet fa-2x text-gray-300"></i>
-            </div>
-
-          </div>
-          <a href="" class="small-box-footer">
-
-            <i class="fa fa-arrow-circle-right"></i>
-          </a>
-        </div>
-      </div>
-    </div>
   </div>
 
   <!-- Content Row -->
