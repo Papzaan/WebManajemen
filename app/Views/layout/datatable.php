331cda7a161570eb06a9a28b3ae21316d271120f
Aan Sanova <37663944+Papzaan@users.noreply.github.com>
2021-10-05 08:54:42 +0700

    Update datatable.php

--- app/Views/layout/datatable.php
+++ app/Views/layout/datatable.php
@@ -3,9 +3,15 @@
 <link href="<?php base_url(); ?>/assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 <script src="<?php base_url(); ?>/assets/vendor/datatables/jquery.dataTables.min.js"></script>
 <script src="<?php base_url(); ?>/assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+
+<!-- <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/rowreorder/1.2.8/css/rowReorder.dataTables.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css"> -->
 <!--  -->
 <!-- <script src="<?php base_url(); ?>/assets/js/jquery-3.5.1.js"></script> -->
 <!-- <script src="<?php base_url(); ?>/assets/js/jquery.dataTables.min.js"></script> -->
+<!-- <script src="https://cdn.datatables.net/rowreorder/1.2.8/js/dataTables.rowReorder.min.js"></script>
+<script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script> -->
 <script src="<?php base_url(); ?>/assets/js/dataTables.buttons.min.js"></script>
 <script src="<?php base_url(); ?>/assets/js/jszip.min.js"></script>
 <script src="<?php base_url(); ?>/assets/js/pdfmake.min.js"></script>
